ae88c4db4a76c109e518e8dd61559802614d8300
Yichen Liang <liangyichen666@gmail.com>
2022-06-19 11:45:13 +0100

    commentary updated

--- simulator.py
+++ simulator.py
@@ -1,57 +1,60 @@
-import time
-import numpy as np
 import math
+import numpy as np
+import time
 
 from PySide2.QtWidgets import QApplication, QMessageBox
 from PySide2.QtUiTools import QUiLoader
+from PySide2.QtCore import Signal, QObject, Qt
 import pyqtgraph as pg
 from threading import Thread
-from PySide2.QtCore import Signal, QObject, Qt
 
-from Mobile_Robot import Robot
+from model_mobile_robot import ClassRobot
 
 
-# 自定义信号源对象类型，一定要继承自 QObject
+# Define signal object, inherit from QObject
 class MySignals(QObject):
-    # define Signal
-    # 调用 emit方法 发信号时，传入参数 必须是这里指定的 参数类型
-    plot = Signal(np.ndarray, pg.ArrowItem, np.ndarray, pg.PlotItem)
-    finished = Signal(str)
+    # When calling the emit method to send a signal,
+    # the incoming parameter must be the parameter type specified here
+    data_signal = Signal(np.ndarray, pg.ArrowItem, np.ndarray, pg.PlotItem)
+    finished_signal = Signal(str)
 
 
-class Simulator:
+class Simulator(object):
     def __init__(self):
         loader = QUiLoader()
         # registe PlotWidget
         loader.registerCustomWidget(pg.PlotWidget)
         self.ui = loader.load("main.ui")
-        self.ui.runButton.clicked.connect(self.handleRun)
-        self.ui.stopButton.clicked.connect(self.handleStop)
-        self.ui.resetButton.clicked.connect(self.handleReset)
         self.ui.Widget_plot.setYRange(-2, 2)
         self.ui.Widget_plot.setXRange(-2, 2)
 
-        self.ms = MySignals()
-        self.ms.plot.connect(self.Plot)
-        self.ms.finished.connect(self.finished)
+        self.ui.runButton.clicked.connect(self.handleRun)
+        self.ui.stopButton.clicked.connect(self.handleStop)
+        self.ui.resetButton.clicked.connect(self.handleReset)
 
-        self.robot = Robot()
+        self.ms = MySignals()
+        self.ms.data_signal.connect(self.Plot)
+        self.ms.finished_signal.connect(self.finished)
+
+        self.robot = ClassRobot()
         self.posedata = self.robot.pose
         self.posedata_estimated = self.robot.pose_estimated
 
-        self.Thread_stop = False
+        self.Thread_stop = False  # flag to stop the robot run thread
 
     def Plot(self, posedata, arrow, posedata_estimated, ellipse):
         self.ui.Widget_plot.clear()
+
         self.ui.Widget_plot.plot(posedata[:, 0], posedata[:, 1])
         self.ui.Widget_plot.addItem(arrow)
 
-        mypen = pg.mkPen(color="g", style=Qt.DashLine)  # Dotted pale-blue line
+        mypen = pg.mkPen(color="g", style=Qt.DashLine)  # Dashed green line
         self.ui.Widget_plot.plot(
             posedata_estimated[:, 0], posedata_estimated[:, 1], pen=mypen
         )
         self.ui.Widget_plot.addItem(ellipse)
 
+        # update location data
         self.ui.label_x.setText(str(posedata[-1, 0])[0:5])
         self.ui.label_y.setText(str(posedata[-1, 1])[0:5])
         self.ui.label_x_estimation.setText(str(posedata_estimated[-1, 0])[0:5])
@@ -100,7 +103,7 @@ class Simulator:
                     k = math.sqrt((qyy - qxx) ** 2 + 4 * (qxy) ** 2)
                     quu = (qyy + qxx + k) / 2
                     qvv = (qyy + qxx - k) / 2
-                    s = 5.991  # 95％置信区间对应于s=5.991
+                    s = 5.991  # 95％ confidence interval corrpreating s=5.991
                     a = s * math.sqrt(quu)
                     b = s * math.sqrt(qvv)
                     e = math.atan2(2 * qxy, (qyy - qxx)) / 2
@@ -108,17 +111,17 @@ class Simulator:
                     # print('qxx=',qxx,'qyy=',qyy,'qxy=',qxy,'\n','k=',k,'quu=',quu,'qvv=',qvv,'\n',a,b,e)
                     ex_data, ey_data = get_ellipse(ellipse_x, ellipse_y, a, b, -e)
                     ErrorEllipse = pg.PlotCurveItem(ex_data, ey_data)
-                    self.ms.plot.emit(
+                    self.ms.data_signal.emit(
                         self.posedata, arrow, self.posedata_estimated, ErrorEllipse
                     )
                     i = 0
                 time.sleep(0.005)
                 i += 1
                 if dt <= 0.1:
-                    self.ms.finished.emit("Finished")
+                    self.ms.finished_signal.emit("Finished")
                     break
                 if self.Thread_stop == True:
-                    self.ms.finished.emit("Stop")
+                    self.ms.finished_signal.emit("Stop")
                     break
 
         t = Thread(target=Run)
@@ -136,17 +139,17 @@ class Simulator:
 
 
 def get_ellipse(e_x, e_y, a, b, e_angle):
-    """[summary]
-    获取椭圆轨迹
+    """
+    Get elliptical trajectory
     Args:
-            e_x ([type]): [圆心x]
-            e_y ([type]): [圆心y]
-            a ([type]): [长轴]
-            b ([type]): [短轴]
-            e_angle ([type]): [旋转角度]]
+            e_x ([type]): [center x]
+            e_y ([type]): [center y]
+            a ([type]): [long axis]
+            b ([type]): [short axis]
+            e_angle ([type]): [Rotation angle]]
 
     Returns:
-            [type]: [x, y的轨迹]
+            [list]: [trajectory of x,y]
     """
     angles_circle = np.arange(0, 2 * np.pi, 0.01)
     x = []
